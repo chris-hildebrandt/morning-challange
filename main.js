fdc91e699782cd8267d06501a3faca7c5f8554ac
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-10 09:52:54 -0600

    battle for middle earth

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ console.log("javascript running");
 function mostCommonLetter(str) {
   // let duplicates = {char, frequency}
   let alphabet = {}
-  for (i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     let letter = str[i]
     if (alphabet[letter]) {
       alphabet[letter]++
@@ -43,13 +43,13 @@ function mostCommonLetter(str) {
   }
   let letters = []
   alphabet.forEach(letter => letters.push(letter[value]))
-  letters.sort((a,b) => a-b)
-  
+  letters.sort((a, b) => a - b)
+
 }
 
 function factorialize(num) {
   let factors = []
-  for (i = 1; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     if (num % i == 0) {
       factors.push(i)
     }
@@ -57,3 +57,34 @@ function factorialize(num) {
   return (factors)
 }
 
+function battleForMiddleEarth(strG, strE) {
+  let arrG = strG.split(' ')
+  let goodTeam = (Number(arrG[0] * 1) + (arrG[1] * 2) + (arrG[2] * 3) + (arrG[3] * 3) + (arrG[4] * 4) + (arrG[5] * 10))
+
+  let arrE = strE.map(s => parseInt(s))
+  let evilTeam = (parseInt(arrE[0]) + (arrE[1] * 2) + (arrE[2] * 2) + (arrE[3] * 2) + (arrE[4] * 3) + (arrE[5] * 5) + (arrE[6] * 10))
+  if (evilTeam > goodTeam) {
+    return "evil team wins"
+  } else if (evilTeam = goodTeam) {
+    return "tie"
+  } else {
+    return "good team wins!"
+  }
+}
+
+function betterMethod(strG, strE) {
+
+  let goodSide = [1, 2, 3, 3, 4, 10]
+  let badSide = [1, 2, 2, 2, 3, 5, 10]
+
+  let goodTotal = 0
+
+  for (let i = 0; i < strG.length; i++) {
+    let troopType = strG[i]
+    let multiplier = goodSide[i]
+    goodTotal += troopType * multiplier
+  }
+
+  let badTotal = 0
+  badSide.forEach((t, i) => { badTotal += strE[i * t] })
+}
